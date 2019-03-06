303dfd01d0cfec71e16325ab3aad5d0b9e60aced
Ali Hasan <ahb7671@gmail.com>
2019-03-06 05:32:23 +0000

    Drivers: Fix merge derp
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- drivers/misc/uid_sys_stats.c
+++ drivers/misc/uid_sys_stats.c
@@ -459,16 +459,6 @@ static const struct file_operations uid_remove_fops = {
 	.write		= uid_remove_write,
 };
 
-#if IS_ENABLED(CONFIG_TASK_IO_ACCOUNTING)
-static u64 compute_write_bytes(struct task_struct *task)
-{
-	if (task->ioac.write_bytes <= task->ioac.cancelled_write_bytes)
-		return 0;
-
-	return task->ioac.write_bytes - task->ioac.cancelled_write_bytes;
-}
-#endif
-
 #if IS_ENABLED(CONFIG_TASK_IO_ACCOUNTING) || IS_ENABLED(CONFIG_TASK_XACCT)
 static void add_uid_io_stats(struct uid_entry *uid_entry,
 			struct task_struct *task, int slot)
